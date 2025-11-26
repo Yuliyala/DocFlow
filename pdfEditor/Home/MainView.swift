6092bb69a3aef8bf1d396e80c96e8721d9325675
Yuliyala <yuliyalapenak@gmail.com>
2025-11-26 14:30:09 +0300

    fix offset

--- pdfEditor/Home/MainView.swift
+++ pdfEditor/Home/MainView.swift
@@ -184,6 +184,7 @@ class HomeView: UIView {
         scrollView.snp.makeConstraints {
             $0.top.equalTo(searchBar.snp.bottom).offset(20)
             $0.left.right.bottom.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(50)
         }
         
         contentStackView.snp.makeConstraints {
